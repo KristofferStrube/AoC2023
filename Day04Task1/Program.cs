6fe86a9e5b9f9917b48a273cd638c4cd3e049571
KristofferStrube <kristoffer.strube@gmail.com>
2023-12-04 06:38:21 +0100

    Added solutions for day 4

---
+++ Day04Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿var cards = Day04Task1.Input.Literal
+    .Split("\r\n")
+    .Select(line => line.Split(":"))
+    .Select(lineParts => (start: lineParts[0], numberSections: lineParts[1].Split(" |")))
+    .Select(lineParts => (
+        id: lineParts.start.Split(" ")[1],
+        winning: lineParts.numberSections[0].Chunk(3).Select(n => new string(n)),
+        yours: lineParts.numberSections[1].Chunk(3).Select(n => new string(n))
+    ))
+    .ToArray();
+
+var points = cards.Sum(card =>
+    card.yours.Count(your => card.winning.Contains(your)) is { } corrects and >= 1 ? Math.Pow(2, corrects - 1) : 0
+);
+
+Console.WriteLine(points);
---
+++ Day04Task2/Program.cs
@@ -0,0 +1,29 @@
+﻿var cards = Day04Task1.Input.Literal
+    .Split("\r\n")
+    .Select(line => line.Split(":"))
+    .Select(lineParts => (start: lineParts[0], numberSections: lineParts[1].Split(" |")))
+    .Select(lineParts => (
+        id: int.Parse(new string(lineParts.start.Skip(5).ToArray())),
+        winning: lineParts.numberSections[0].Chunk(3).Select(n => new string(n)),
+        yours: lineParts.numberSections[1].Chunk(3).Select(n => new string(n))
+    ))
+    .ToArray();
+
+Dictionary<int, int> cardCounts = new(cards.Length);
+foreach (var card in cards)
+{
+    cardCounts.Add(card.id, 1);
+}
+
+foreach (var card in cards)
+{
+    if (card.yours.Count(your => card.winning.Contains(your)) is { } corrects and >= 1)
+    {
+        for (int i = 0; i < corrects; i++)
+        {
+            cardCounts[card.id + i + 1] += cardCounts[card.id];
+        }
+    }
+}
+
+Console.WriteLine(cardCounts.Values.Sum());
