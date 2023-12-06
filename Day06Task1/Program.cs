3cdb954a9e434a6cece695c498e182da0b03afbd
KristofferStrube <kristoffer.strube@gmail.com>
2023-12-06 06:31:56 +0100

    Added solutions for day 6

---
+++ Day06Task1/Program.cs
@@ -0,0 +1,21 @@
+﻿(int time, int distance)[] races = new[] { (49, 356), (87, 1378), (78, 1502), (95, 1882) };
+
+int multSum = 1;
+
+foreach(var race in races)
+{
+    int solutions = 0;
+    int lastSolution = 0;
+    int hold = 0;
+    while (true)
+    {
+        int currSolution = (race.time - hold) * hold;
+        if (currSolution < lastSolution && currSolution < race.distance) break;
+        if (currSolution > race.distance) solutions++;
+        lastSolution = currSolution;
+        hold++;
+    }
+    multSum *= solutions;
+}
+
+Console.WriteLine(multSum);
---
+++ Day06Task2/Program.cs
@@ -0,0 +1,17 @@
+﻿//(int time, int distance)[] races = new[] { (7, 9), (15, 40), (30, 200) };
+//(long time, long distance)[] races = new[] { ((long)71530, (long)940200) };
+(long time, long distance) race = (49877895, 356137815021882);
+
+long solutions = 0;
+long lastSolution = 0;
+long hold = 0;
+while (true)
+{
+    long currSolution = (race.time - hold) * hold;
+    if (currSolution < lastSolution && currSolution < race.distance) break;
+    if (currSolution > race.distance) solutions++;
+    lastSolution = currSolution;
+    hold++;
+}
+
+Console.WriteLine(solutions);
