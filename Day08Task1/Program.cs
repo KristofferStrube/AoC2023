b31b348cba6ce6edabb72c2d16ea8e244e5c01c1
KristofferStrube <kristoffer.strube@gmail.com>
2023-12-10 22:12:24 +0100

    added solution for day 8 task 1

---
+++ Day08Task1/Program.cs
@@ -0,0 +1,20 @@
+﻿var lines = Day08Task1.Input.Literal
+    .Split("\r\n");
+
+var instructions = lines[0];
+
+var nodes = lines
+    .Skip(2)
+    .Select(line => line.Split(" = "))
+    .ToDictionary(lineParts => lineParts[0], lineParts => lineParts[1][1..^1].Split(", "));
+
+var position = "AAA";
+int inst = 0;
+
+while(position is not "ZZZ")
+{
+    position = nodes[position][instructions[inst % instructions.Length] is 'L' ? 0 : 1];
+    inst++;
+}
+
+Console.WriteLine(inst);
