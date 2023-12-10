8aa689515ac53c09c5ffc499d15faa6d79e3fff4
KristofferStrube <kristoffer.strube@gmail.com>
2023-12-10 22:13:12 +0100

    Added solutions for Day 9

---
+++ Day09Task1/Program.cs
@@ -0,0 +1,26 @@
+﻿var lines = Input.Literal
+    .Split("\r\n");
+
+int sum = 0;
+
+foreach(var line in lines)
+{
+    List<int[]> differences = new();
+    differences.Add(line.Split(" ").Select(int.Parse).ToArray());
+    while(differences.Last().Any(n => n is not 0))
+    {
+        var last = differences.Last();
+        differences.Add(Enumerable
+            .Range(1, last.Length - 1)
+            .Select(i => last[i] - last[i - 1])
+            .ToArray());
+    }
+    int extrapolated = 0;
+    for (int i = differences.Count - 2; i >= 0; i--)
+    {
+        extrapolated = differences[i][^1] + extrapolated;
+    }
+    sum += extrapolated;
+}
+
+Console.WriteLine(sum);
---
+++ Day09Task1/SampleInput.cs
@@ -0,0 +1,9 @@
+﻿public class SampleInput
+{
+    public const string Literal =
+"""
+0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45
+""";
+}
