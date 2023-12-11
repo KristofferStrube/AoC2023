5a794d3215ecd46ef8eefafb6f5ce8e8f8081660
KristofferStrube <kristoffer.strube@gmail.com>
2023-12-12 00:03:53 +0100

    Added solutions for day 11

---
+++ Day11Task1/Program.cs
@@ -0,0 +1,43 @@
+﻿var grid = Input.Literal
+    .Split("\r\n");
+
+var emptyRows = Enumerable
+    .Range(0, grid.Length)
+    .Where(row => !Enumerable.Range(0, grid[0].Length).Any(column => grid[row][column] is '#'))
+    .ToArray();
+
+var emptyColumns = Enumerable
+    .Range(0, grid[0].Length)
+    .Where(column => !Enumerable.Range(0, grid.Length).Any(row => grid[row][column] is '#'))
+    .ToArray();
+
+List<(int y, int x)> galaxies = new();
+
+for (int i = 0; i < grid.Length; i++)
+{
+    for (int j = 0; j < grid[0].Length; j++)
+    {
+        if (grid[i][j] is '#')
+        {
+            galaxies.Add((i, j));
+        }
+    }
+}
+
+int sum = 0;
+
+for (int i = 0; i < galaxies.Count; i++)
+{
+    for (int j = 0; j < i; j++)
+    {
+        var galaxy1 = galaxies[i];
+        var galaxy2 = galaxies[j];
+        var dist = Math.Abs(galaxy2.x - galaxy1.x) + Math.Abs(galaxy2.y - galaxy1.y);
+        dist += emptyRows.Count(r => r < Math.Max(galaxy1.y, galaxy2.y) && r > Math.Min(galaxy1.y, galaxy2.y));
+        dist += emptyColumns.Count(c => c < Math.Max(galaxy1.x, galaxy2.x) && c > Math.Min(galaxy1.x, galaxy2.x));
+        sum += dist;
+    }
+}
+
+
+Console.WriteLine(sum);
